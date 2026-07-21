f0d6b4723db802a0511b543b9c28e7beba4d1f56
toyib <toyibxzz@gmail.com>
2026-07-21 18:23:10 +0700

    rubah vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
-  base: '/crud/'
-})
+  base: command === 'build' ? '/crud/' : '/'
+}))
